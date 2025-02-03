a2e1fe76ac04f818122de99e57e7de3db0b0949d
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-02-03 00:54:26 -0500

    Added basic XML parser.

--- scripts/load-dbgap-xml.js
+++ scripts/load-dbgap-xml.js
@@ -2,15 +2,29 @@
  * A script for loading dbGaP XML files into a DuckDB database that can be loaded by Serval.
  */
 
+import * as fs from 'fs';
 import { program } from 'commander';
+import { XMLParser } from 'fast-xml-parser';
 
 program
+    .option('--duckdb <duckdb-db-path>', 'DuckDB database to load into', 'duckdb.duckdb')
     .argument('<dbgap-xml-dir-or-file>', 'Directory or file containing dbGaP XML files');
 
 program.parse();
 
+const { duckdb } = program.opts();
 const [ dbgapXmlDirOrFile ] = program.args;
 
 // TODO: fancy code to turn dbGaPXmlDirOrFile into a list of files.
 
-console.log(`Reading dbGaP XML files from ${dbgapXmlDirOrFile}`);
+console.log(`Loading dbGaP XML files from ${dbgapXmlDirOrFile} into DuckDB database ${duckdb}.`);
+
+const fileAsString = fs.readFileSync(dbgapXmlDirOrFile).toString()
+const parser = new XMLParser();
+const parsed = parser.parse(fileAsString);
+const data_table = parsed['data_table'];
+const variables = data_table['variable'];
+
+for (const variable of variables) {
+    console.log('Variable: ', variable);
+}
