c4c6fc9635e6456d31a041f2e7c6f49a0b8dee49
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-02-03 00:46:35 -0500

    First stab at a command-line script.

--- scripts/load-dbgap-xml.js
+++ scripts/load-dbgap-xml.js
@@ -2,4 +2,15 @@
  * A script for loading dbGaP XML files into a DuckDB database that can be loaded by Serval.
  */
 
-console.log("What");
+import { program } from 'commander';
+
+program
+    .argument('<dbgap-xml-dir-or-file>', 'Directory or file containing dbGaP XML files');
+
+program.parse();
+
+const [ dbgapXmlDirOrFile ] = program.args;
+
+// TODO: fancy code to turn dbGaPXmlDirOrFile into a list of files.
+
+console.log(`Reading dbGaP XML files from ${dbgapXmlDirOrFile}`);
