90c929d1861f9925f684be19a9637aaa915ce5f1
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-10-24 00:06:00 -0400

    Added a basic setup with Bootstrap, URLs and Vue components.

---
+++ src/components/DisplayAnnotations.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  urls: string[]
+}>()
+</script>
+
+<template>
+  <h2>URLs</h2>
+  <ul>
+    <li v-for="url in urls" :key="url">
+      <a :href="url" target="_blank">{{ url }}</a>
+    </li>
+  </ul>
+</template>
+
+<style>
+</style>
