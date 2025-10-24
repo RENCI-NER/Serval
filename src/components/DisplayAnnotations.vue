957be478473b0f27ac62a79df27eca555491244d
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-10-24 09:46:50 -0400

    Now we can display URLs and annotations.

--- src/components/DisplayAnnotations.vue
+++ src/components/DisplayAnnotations.vue
@@ -4,6 +4,7 @@ import {onMounted, ref} from "vue";
 defineProps<{
 }>()
 
+// 1. Get all URLs from the query string.
 const urls = new URLSearchParams(window.location.search).getAll('url');
 
 // 2. Reactive map: url → parsed JSONL array
@@ -25,16 +26,42 @@ onMounted(async () => {
       urls.map(async (url) => [url, await fetchJsonl(url)])
   );
   // Convert array of [url, data] → object like { url: data }
-  urlData.value = Object.fromEntries(entries);
+  urlData.value = Object.fromEntries(entries.map(entry => {
+    const url = entry[0] as string;
+    const lines = entry[1] as object[];
+
+    return [url, lines.map(data => ({
+      ...data,
+      url,
+
+      // Come up with a title based on the first 100 characters of the text.
+      // TODO: renci-ner should produce this itself!
+      // Plus GUIDs maybe?
+      title: data.text.substring(0, 100),
+    }))];
+  }));
 });
+
 </script>
 
 <template>
   <h2>URLs</h2>
   <ul>
-    <li v-for="url in urls" :key="url">
-      <a :href="url" target="_blank">{{ url }}</a>: {{ urlData[url] ? urlData[url].length : '[loading]' }} annotations
-    </li>
+    <template v-for="url in urls" :key="url">
+      <template v-if="url in urlData">
+        <li>
+          <a :href="url" target="_blank">{{ url }}</a>: {{ urlData[url].length }} entries
+        </li>
+        <ul>
+          <ul v-for="entry in urlData[url]" :key="entry.title">
+            <code>{{entry.title}}</code>
+          </ul>
+        </ul>
+      </template>
+      <template v-else>
+        <li><a :href="url" target="_blank">{{url}}</a>: Loading...</li>
+      </template>
+    </template>
   </ul>
 </template>
 
